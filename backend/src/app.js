4fae74d58d9976675b54fefaaf4391a61c2ac336
Pedro Chaves <pedrochavesjf@gmail.com>
2022-07-04 10:29:43 -0300

    fix: arrumando o app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,6 @@ const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
-
-app.get('/', (_req, res) => {
-  res.status(200).send('entrou');
-});
 app.use(routes.tasks);
 
 module.exports = app;
